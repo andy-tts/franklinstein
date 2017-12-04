2bab5c9a4df58e60664938de08fa82094d63eec4
Jade D. Kazan <jadekzn@gmail.com>
2017-12-04 13:33:20 -0500

    Showing my fiancee how I add pictures to the magic search view

--- app/controllers/magic_controller.rb
+++ app/controllers/magic_controller.rb
@@ -2,10 +2,12 @@ class MagicController < ApplicationController
 
 def index
 	@all_cards = []
+	@image = []
 	if params[:name].present?
 		results = HTTParty.get("http://api.magicthegathering.io/v1/cards?name=#{params[:name]}")
 		results['cards'].each do |card|
-			@all_cards.push(card['name'])
+			full_card = [card['name'], card['imageUrl']]
+			@all_cards.push(full_card)
 		end
 	end
 end
