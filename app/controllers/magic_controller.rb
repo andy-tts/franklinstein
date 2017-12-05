013ae90187e92a65f3069535a67f7b5b6ec7189c
Jade Kazoo <jadekzn@gmail.com>
2017-12-05 12:30:25 -0500

    Add ability to deal with card not found in compare

--- app/controllers/magic_controller.rb
+++ app/controllers/magic_controller.rb
@@ -28,36 +28,52 @@ def compare
 	@card1_info = []
 	@card2_info = []
 	img_test = nil
+	none1 = false
+	none2 = false
 	if params[:card1].present? && params[:card2].present?
 		@card1 = params[:card1]
 		@card2 = params[:card2]
 		first_card = HTTParty.get("http://api.magicthegathering.io/v1/cards?name=#{params[:card1]}")
 		second_card = HTTParty.get("http://api.magicthegathering.io/v1/cards?name=#{params[:card2]}")
 
-		first_card['cards'].each do |card|
-			if card['name'] == params[:card1]
-				img_test = [1, card['imageUrl']]
-				if img_test[1].present? && caught1 == false
-					@card1_info = [card['name'], card['imageUrl'], card['cmc'], card['types'], card['subtypes'], card['rarity'], card['legalities'], card['type'], card['colorIdentity']]
-					if @card1_info[8].present? == false
-						@card1_info[8] = ["Colorless"]
+		if  first_card['cards'] == []
+			@card1_info = ["[card not found]", "/assets/mtg_back.jpg", "0", [], [], [], [{"format":"Reality","legality":"Banned"}], "N/A", ["Colorlesss"]]
+			none1 = true
+		end
+
+		if none1 == false
+			first_card['cards'].each do |card|
+				if card['name'] == params[:card1]
+					img_test = [1, card['imageUrl']]
+					if img_test[1].present? && caught1 == false
+						@card1_info = [card['name'], card['imageUrl'], card['cmc'], card['types'], card['subtypes'], card['rarity'], card['legalities'], card['type'], card['colorIdentity']]
+						if @card1_info[8].present? == false
+							@card1_info[8] = ["Colorless"]
+						end
+						caught1 = true
 					end
-					caught1 = true
 				end
 			end
 		end
 
 		img_test = nil
 
-		second_card['cards'].each do |card|
-			if card['name'] == params[:card2]
-				img_test = [1, card['imageUrl']]
-				if img_test[1].present? && caught2 == false
-					@card2_info = [card['name'], card['imageUrl'], card['cmc'], card['types'], card['subtypes'], card['rarity'], card['legalities'], card['type'], card['colorIdentity']]
-					if @card2_info[8].present? == false
-						@card2_info[8] = ["Colorless"]
+		if  second_card['cards'] == []
+			@card2_info = ["[card not found]", "/assets/mtg_back.jpg", "0", [], [], [], [{"format":"Reality","legality":"Banned"}], "N/A", ["Colorlesss"]]
+			none2 = true
+		end
+
+		if none2 == false
+			second_card['cards'].each do |card|
+				if card['name'] == params[:card2]
+					img_test = [1, card['imageUrl']]
+					if img_test[1].present? && caught2 == false
+						@card2_info = [card['name'], card['imageUrl'], card['cmc'], card['types'], card['subtypes'], card['rarity'], card['legalities'], card['type'], card['colorIdentity']]
+						if @card2_info[8].present? == false
+							@card2_info[8] = ["Colorless"]
+						end
+						caught2 = true
 					end
-					caught2 = true
 				end
 			end
 		end
