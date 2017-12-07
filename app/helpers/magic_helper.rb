f8d4b852490d593848ecfbec79ffc432f291074e
Jade Kazoo <jadekzn@gmail.com>
2017-12-07 11:03:51 -0500

    added information parsing method to helper

--- app/helpers/magic_helper.rb
+++ app/helpers/magic_helper.rb
@@ -21,6 +21,81 @@ module MagicHelper
 		return new_cards
 	end
 
+	def various_info_to_s(card)
+		break_array = []
+		color = ""
+		colors = ""
+		first = true
+		break_array = card.color_identity.chars
+		break_array.each do |char|
+			if char == "R" || char == "B" || char == "U" || char == "G" || char == "W" || char === "C"
+				if char == "R"
+					color = "Red"
+				elsif char == "B"
+					color = "Black"
+				elsif char == "U"
+					color = "Blue"
+				elsif char == "G"
+					color = "Green"
+				elsif char == "W"
+					color = "White"
+				elsif char == "C"
+					color = "Colorless"
+				end
+				if first == true
+					colors = colors + color
+					first = false
+				elsif first == false
+					colors = colors + ", " + color
+				end
+			end
+		end
+
+		info = ""
+		formats = []
+		legalities = []
+		legal_format = false
+		legal_legality = false
+		break_array = card.legalities.split("")
+		last = break_array.length
+		break_array.each_with_index do |char, index|
+			if char != "\"" && char != "[" && char != "]" && char != "{" && char != "}" && char != "=" && char != ">"# && char != ","
+				info = info + char
+			end
+			if info == "format"
+				info = ""
+				legal_format = true
+			elsif info.end_with?(", ") && legal_format == true
+				info.chop!
+				info.chop!
+				formats.push(info)
+				info = ""
+				legal_format = false
+			elsif info == "legality"
+				info = ""
+				legal_legality = true
+			elsif info.end_with?(", ") && legal_legality == true
+				info.chop!
+				info.chop!
+				legalities.push(info)
+				info = ""
+				legal_legality = false
+			elsif last == index+1 && legal_legality == true
+				legalities.push(info)
+				info = ""
+			end
+		end
+
+		info = ""
+
+		formats.each_with_index do |formatt, index|
+			info = info + " <h4> #{formatt}:</h4> <p>#{legalities[index]} </p> "
+		end
+		card.legalities = info
+		card.color_identity = colors
+		return card
+	end
+
 	def single_card(results, card_name)
 			exists = false
 			none = false
@@ -56,13 +131,14 @@ module MagicHelper
 						if card['imageUrl'].present? && caught == false
 							compare_card = Card.new(name: card['name'], mana_cost: card['manaCost'], cmc: card['cmc'], card_type: card['type'], artist: card['artist'], img_url: card['imageUrl'], set_name: card['setName'], color_identity: card['colorIdentity'].to_s, legalities: card['legalities'].to_s, sub_types: card['subtypes'].to_s, rarity: card['rarity'])
 							if compare_card.color_identity.present? == false
-								compare_card.color_identity = "Colorless"
+								compare_card.color_identity = "C"
 							end
 							caught = true
 						end
 					end
 				end
 			end
+			compare_card = various_info_to_s(compare_card)
 			return compare_card
 	end
 
